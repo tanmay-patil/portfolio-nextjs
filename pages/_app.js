6b021e91dcf0217a013e209f867aec1193ad8411
Tanmay Patil <tanmay.patil7565@gmail.com>
2022-05-01 19:11:08 +0530

    feat(*): add NeueMontreal font, navbar, foorter layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,15 @@
-// import { ThemeProvider } from "next-themes";
-// import "./app.css";
+import { ThemeProvider } from "next-themes";
+import Layout from "../components/layout";
+import "./global.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <ThemeProvider>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </Layout>
+  );
 }
 
 export default MyApp;
